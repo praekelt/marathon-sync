14c4b9840fadc9f8c10828f8bb0be09d986d3a13
Jamie Hewland <jhewland@gmail.com>
2015-08-25 13:30:45 +0200

    Move requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'requirements.txt')) as f:
-    requires = filter(None, f.readlines())
-
 with open(os.path.join(here, 'VERSION')) as f:
     version = f.read().strip()
 
@@ -25,8 +22,10 @@ setup(name='marathon-sync',
       packages=find_packages(exclude=['docs']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
+      install_requires=[
+          "Twisted",
+          "treq",
+      ],
       entry_points={
           'console_scripts': ['marathon-sync = marathon_sync.cli:entry_point'],
       })
